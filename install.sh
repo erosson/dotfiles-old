bd2fa9d4ed9d1d10d3191f810d4ea6557c84a2a1
Evan Rosson <github@erosson.org>
2014-12-25 08:14:44 +0000

    usage

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -eux
+# usage:
+#
+#    curl https://raw.githubusercontent.com/erosson/dotfiles/master/install.sh | sh
 sudo apt-get update
 sudo apt-get install git
 rm -rf dotfiles ; git clone https://github.com/erosson/dotfiles
