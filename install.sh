a3265b2adb99f19bc7e24113f38b458b6dbc073f
Evan Rosson <github@erosson.org>
2014-12-25 08:14:02 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install git
 rm -rf dotfiles ; git clone https://github.com/erosson/dotfiles
 for f in `ls -A dotfiles/`; do
-	if [ "$f" == "install.sh" ]; then
+	if [ "$f" != "install.sh" ]; then
 		ln -s dotfiles/$f || echo "skipped $f"
 	fi
 done
