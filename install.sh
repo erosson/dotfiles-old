06bb1c64a695e42f10d378313ad9ef66d627fd20
Evan Rosson <github@erosson.org>
2014-12-25 08:19:26 +0000

    exclude .git

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install git
 rm -rf dotfiles ; git clone https://github.com/erosson/dotfiles
 for f in `ls -A dotfiles/`; do
-	if [ "$f" != "install.sh" ]; then
+	if [ "$f" != "install.sh" -a "$f" != ".git" ]; then
 		ln -s dotfiles/$f || echo "skipped $f"
 	fi
 done
