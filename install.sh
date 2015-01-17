1e5fc0a61a42e93bf759383f726dcaa2118a8a3b
Evan Rosson <github@erosson.org>
2015-01-16 20:20:36 -0800

    git email, more package installs

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh -eux
 # usage:
 #
-#    curl https://raw.githubusercontent.com/erosson/dotfiles/master/install.sh | sh
+#    wget -O - https://raw.github.com/erosson/dotfiles/master/install.sh | sh
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install git -y
 rm -rf dotfiles ; git clone https://github.com/erosson/dotfiles
 for f in `ls -A dotfiles/`; do
 	if [ "$f" != "install.sh" -a "$f" != ".git" ]; then
 		ln -s dotfiles/$f || echo "skipped $f"
 	fi
 done
+git config --global user.name 'Evan Rosson'
+# rot13 email, because spambots. http://stackoverflow.com/questions/5442436/using-rot13-and-tr-command-for-having-an-encrypted-email-address
+git config --global user.email `echo tvguho@rebffba.bet | tr '[A-Za-z]' '[N-ZA-Mn-za-m]'` 
+
+sudo apt-get install zsh screen vim -y
