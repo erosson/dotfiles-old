69cf4ff9f741243f499c3e30f551b0e98ca07b6d
Evan Rosson <github@erosson.org>
2015-01-16 20:46:19 -0800

    ssh clone

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #    wget -O - https://raw.github.com/erosson/dotfiles/master/install.sh | sh
 sudo apt-get update
 sudo apt-get install git -y
-rm -rf dotfiles ; git clone https://github.com/erosson/dotfiles
+rm -rf dotfiles ; git clone git@github.com:erosson/dotfiles.git
 for f in `ls -A dotfiles/`; do
 	if [ "$f" != "install.sh" -a "$f" != ".git" ]; then
 		ln -s dotfiles/$f || echo "skipped $f"
