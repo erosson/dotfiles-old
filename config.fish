436019d89df152ce2883351eb3d03916d0064897
Evan <github@erosson.org>
2017-10-22 11:16:10 -0400

    gnu screen titles

--- config.fish
+++ config.fish
@@ -1,1 +1,12 @@
 set PATH (yarn global bin) $PATH
+
+# https://stackoverflow.com/a/25200448
+# gnu screen titles
+function preexec --on-event fish_preexec
+  set dargv $argv "" ""
+  #echo "hello $dargv[1] $dargv[2]"
+  echo -ne "\ek$dargv[1] $dargv[2]\e\\"
+end
+function postexec --on-event fish_postexec
+  echo -ne '\ekfish\e\\'
+end
