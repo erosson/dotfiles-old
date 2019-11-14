bd8019bd9c4647155be1c88a9b09b58e1544f9e2
Evan <github@erosson.org>
2019-11-14 12:29:01 -0500

    global gitignore

--- setup/git.sh
+++ setup/git.sh
@@ -4,3 +4,4 @@ git config --global push.followTags true
 git config --global user.name 'Evan'
 # rot13 email, because spambots. http://stackoverflow.com/questions/5442436/using-rot13-and-tr-command-for-having-an-encrypted-email-address
 git config --global user.email `echo tvguho@rebffba.bet | tr '[A-Za-z]' '[N-ZA-Mn-za-m]'` 
+git config --global core.excludesfile ~/.gitignore
