e0b97e760d8bdb1766d596b505235adc95c8153d
Evan <github@erosson.org>
2019-11-25 09:38:01 -0500

    oops, better keybase install

--- setup/keybase.sh
+++ setup/keybase.sh
@@ -2,6 +2,6 @@
 # https://keybase.io/docs/the_app/install_linux
 TMP=`mktemp --suffix=.deb`
 curl https://prerelease.keybase.io/keybase_amd64.deb -o $TMP
-sudo apt install $TMP
+sudo apt install -y $TMP
 rm $TMP
 run_keybase -g
