47e500fa3cdeddf879635d5c68af0225b8290833
Evan <github@erosson.org>
2019-11-26 09:24:45 -0500

    skip clock during tests

--- setup/clock.sh
+++ setup/clock.sh
@@ -3,4 +3,10 @@
 # match the Windows clock, even though Windows is "wrong".
 #
 # http://ubuntuhandbook.org/index.php/2016/05/time-differences-ubuntu-1604-windows-10/
-timedatectl set-local-rtc 1 --adjust-system-clock
+#
+# Also, skip this when testing, as docker doesn't seem to like it
+if [ -z "$CI" ]; then
+  timedatectl set-local-rtc 1 --adjust-system-clock
+else
+  echo "CI env: skipping timedatectl (windows clock)"
+fi
